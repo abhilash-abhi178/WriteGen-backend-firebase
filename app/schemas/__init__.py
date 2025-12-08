578f5530016f649dc5861026a81f03e52aae9c47
Abhilash B S <Abhilashabhi20007@gmail.com>
2025-12-08 16:23:53 +0530

    removed

--- app/schemas/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Application schemas module."""
-
-from . import auth
-from . import generation
-from . import sample
-from . import style
-from . import user
-
-__all__ = ["auth", "generation", "sample", "style", "user"]
