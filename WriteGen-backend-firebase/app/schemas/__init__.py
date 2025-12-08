fb82b05ac8b67e966b3d267af34c8b061b76c576
Abhilash B S <Abhilashabhi20007@gmail.com>
2025-12-08 16:19:22 +0530

    Update __init__.py

--- WriteGen-backend-firebase/app/schemas/__init__.py
+++ WriteGen-backend-firebase/app/schemas/__init__.py
@@ -1,1 +1,9 @@
-# app/models/__init__.py
+"""Application schemas module."""
+
+from . import auth
+from . import generation
+from . import sample
+from . import style
+from . import user
+
+__all__ = ["auth", "generation", "sample", "style", "user"]
